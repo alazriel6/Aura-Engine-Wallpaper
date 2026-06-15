260bc809e71e690b99ab125f5c61ed200179dc7b
Alazriel <alazriel6@gmail.com>
2026-06-15 19:24:22 +0700

    Add active state indicator for sidebar navigation buttons

---
+++ Helpers/StringMatchToBooleanConverter.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace LiveWallpaperApp.Helpers
+{
+    public class StringMatchToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return false;
+
+            return value.ToString().Equals(parameter.ToString(), StringComparison.OrdinalIgnoreCase);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isChecked && isChecked && parameter != null)
+            {
+                return parameter.ToString();
+            }
+            return Binding.DoNothing;
+        }
+    }
+}
